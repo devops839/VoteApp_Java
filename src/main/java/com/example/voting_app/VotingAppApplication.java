f6465e848ca8fc71722e812051e251d9abc18414
Pavan K <pavank839@outlook.com>
2024-11-18 09:24:57 +0000

    Updated VotingAppApplication.java

--- src/main/java/com/example/voting_app/VotingAppApplication.java
+++ src/main/java/com/example/voting_app/VotingAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VotingAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VotingAppApplication.class, args);
     }
