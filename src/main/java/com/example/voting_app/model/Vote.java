4e32da3566abbcb43b159ca8b7fdbe2a19e05a05
Pavan K <pavank839@outlook.com>
2024-11-18 09:25:55 +0000

    Updated Vote.java

--- src/main/java/com/example/voting_app/model/Vote.java
+++ src/main/java/com/example/voting_app/model/Vote.java
@@ -1,5 +1,31 @@
 package com.example.voting_app.model;
 
-public @interface Id {
+public class Vote {
+    private String option;
+    private int count;
 
+    public Vote(String option) {
+        this.option = option;
+        this.count = 0;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void incrementCount() {
+        this.count++;
+    }
 }
