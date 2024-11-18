47a49353b243aa5275dd56d98b88e3bae3339e02
Pavan K <pavank839@outlook.com>
2024-11-18 09:31:56 +0000

    Updated VotingAppApplication.java

--- src/main/java/com/example/voting_app/VotingAppApplication.java
+++ src/main/java/com/example/voting_app/VotingAppApplication.java
@@ -1,32 +1,11 @@
-package com.example.voting_app.repository;
+package com.example.voting_app;
 
-import com.example.voting_app.model.Vote;
-import org.springframework.stereotype.Repository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Repository
-public class VoteRepository {
-
-    private List<Vote> votes = new ArrayList<>();
-
-    public VoteRepository() {
-        // Add some default vote options
-        votes.add(new Vote("Option 1"));
-        votes.add(new Vote("Option 2"));
-        votes.add(new Vote("Option 3"));
-    }
-
-    public List<Vote> getAllVotes() {
-        return votes;
-    }
-
-    public void incrementVote(String option) {
-        for (Vote vote : votes) {
-            if (vote.getOption().equals(option)) {
-                vote.incrementCount();
-            }
-        }
+@SpringBootApplication
+public class VotingAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VotingAppApplication.class, args);
     }
 }
