a9c4910eb033f151c89694286bbc10865f04bf5f
Pavan K <pavank839@outlook.com>
2024-11-18 13:17:28 +0000

    Updated Vote.java

--- src/main/java/com/example/voting_app/model/Vote.java
+++ src/main/java/com/example/voting_app/model/Vote.java
@@ -2,10 +2,9 @@ package com.example.voting_app.model;
 
 public class Vote {
 
-    private String genre; // The movie genre (Action/Adventure, Rom-Com, Horror)
-    private int count;    // The count of votes for this genre
+    private String genre;
+    private int count;
 
-    // Constructor
     public Vote(String genre, int count) {
         this.genre = genre;
         this.count = count;
@@ -26,10 +25,5 @@ public class Vote {
 
     public void setCount(int count) {
         this.count = count;
-    }
-
-    // Method to increment the vote count
-    public void incrementCount() {
-        this.count++;
     }
 }
